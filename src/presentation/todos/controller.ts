dc9704079c4372877c2370b329474c360cad2989
BodemanBoris <borisbodemand@hotmail.com>
2025-06-26 05:52:29 +0200

    adding the DTO todo file

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,14 +1,25 @@
 import { Response, Request } from "express";
+import { prisma } from "../../data/postgres/init";
+import { CreateTodoDto } from "../../domain/dtos/todos";
 
 const arrTasks = [
-  { id: 1, task: "La chain de leyenda", createdAt: new Date() },
-  { id: 2, task: "BORIMAN Sell bike", createdAt: null },
+  { id: 1, task: "Tu vieja", createdAt: new Date() },
+  { id: 2, task: "El raton Boriman", createdAt: null },
   { id: 3, task: "Buy butter", createdAt: new Date() },
 ];
 
 export class TodoController {
   public getTodos(req: Request, res: Response) {
     return res.json(arrTasks);
+  }
+
+  public async getDbTodos(req: Request, res: Response) {
+    try {
+      const tasks = await prisma.todo.findMany();
+      res.status(200).json({ message: "Tasks founded", tasks });
+    } catch (error) {
+      res.status(400).json({ message: "Tasks was not founded" });
+    }
   }
 
   public getTodoById(req: Request, res: Response) {
@@ -22,6 +33,16 @@ export class TodoController {
       return res.status(404).json("Id does'nt exist");
     }
     return res.json(taskById);
+  }
+
+  public async getDbTodoById(req: Request, res: Response) {
+    const taskId = +req.params.taskId;
+    try {
+      const task = await prisma.todo.findUnique({ where: { id: taskId } });
+      res.status(200).json({ message: "Task founded", task });
+    } catch (error) {
+      res.status(404).json({ message: "Task doesn't exist", error });
+    }
   }
 
   public createTask(req: Request, res: Response) {
@@ -45,6 +66,21 @@ export class TodoController {
       message: "A new Task was created",
       arrTasks,
     });
+  }
+
+  public async createDbTask(req: Request, res: Response) {
+    const [error, createTodoDto] = CreateTodoDto.create(req.body);
+
+    if (error) return res.status(400).json({ error });
+    try {
+      await prisma.todo.create({ data: createTodoDto! });
+      return res.status(200).json({ message: "Task was created on Prisma DB" });
+    } catch (error) {
+      console.error("DB Error:", error); // Muy útil para depuración
+      return res
+        .status(500)
+        .json({ message: "Error creating task in PostgreSQL with Prisma" });
+    }
   }
 
   public updateTask(req: Request, res: Response) {
@@ -73,6 +109,26 @@ export class TodoController {
     return res.status(200).json({ message: "Task was updated", arrTasks });
   }
 
+  public async updateDbTask(req: Request, res: Response) {
+    const { task } = req.body;
+    const taskId = +req.params.taskId;
+
+    if (typeof task !== "string" || task.trim().length < 1) {
+      throw new Error("Task is required");
+    }
+    try {
+      const taskUpdated = await prisma.todo.update({
+        where: { id: taskId },
+        data: { text: task },
+      });
+      res.status(200).json({
+        message: "task was updated",
+        beforeTask: task,
+        updatedTask: taskUpdated,
+      });
+    } catch (error) {}
+  }
+
   public deleteTask(req: Request, res: Response) {
     const urlId = +req.params.taskId;
     console.log(urlId);
@@ -82,5 +138,21 @@ export class TodoController {
     return res
       .status(200)
       .json({ message: "Task had been deleted", arrTaskModified });
+  }
+
+  public async deleteDbTask(req: Request, res: Response) {
+    const id = +req.params.taskId;
+
+    if (typeof id !== "number" || id < 0) {
+      throw new Error("'ID' must exist and be a positive number");
+    }
+
+    try {
+      await prisma.todo.delete({ where: { id } });
+      return res.status(200).json({ message: "Task was deleted" });
+    } catch (error) {
+      const customError = `Error: ${error}`;
+      return res.status(400).json({ message: customError });
+    }
   }
 }
--- src/presentation/todos/todo-routes.ts
+++ src/presentation/todos/todo-routes.ts
@@ -7,11 +7,23 @@ export class TodoRoutes {
     const todoController = new TodoController();
 
     router.get("/", (req, res) => todoController.getTodos(req, res));
+    router.get("/db/", (req, res) => todoController.getDbTodos(req, res));
     router.get("/:taskId", (req, res) => todoController.getTodoById(req, res));
+    router.get("/db/:taskId", (req, res) =>
+      todoController.getDbTodoById(req, res)
+    );
     router.post("/", (req, res) => todoController.createTask(req, res));
+    router.post("/db/", (req, res) => todoController.createDbTask(req, res));
     router.put("/:taskId", (req, res) => todoController.updateTask(req, res));
+    router.put("/db/:taskId", (req, res) =>
+      todoController.updateDbTask(req, res)
+    );
     router.delete("/:taskId", (req, res) =>
       todoController.deleteTask(req, res)
+    );
+
+    router.delete("/db/:taskId", (req, res) =>
+      todoController.deleteDbTask(req, res)
     );
 
     return router;
